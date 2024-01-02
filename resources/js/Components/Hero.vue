3bbe77bddac2913ee58a142c13d7b8f905e352b4
Nicro01 <nicro@edu.unifil.br>
2024-01-02 18:37:30 -0300

    Some style changes

--- resources/js/Components/Hero.vue
+++ resources/js/Components/Hero.vue
@@ -130,12 +130,12 @@ export default {
                 </div>
                 <div class="max-w-[800px]">
                     <div
-                        class="relative flex flex-wrap items-stretch sm:w-[400px]"
+                        class="relative flex flex-wrap items-stretch w-full sm:w-[400px]"
                     >
                         <input
                             type="text"
                             v-model="url"
-                            class="relative w-[80%] m-0 -mr-0.5 block min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-green-700 focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-green-700"
+                            class="relative w-[70%] sm:w-[80%] m-0 -mr-0.5 block min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-green-700 focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-green-700"
                             placeholder="Search for a video url..."
                             aria-label="Search for a video url..."
                             aria-describedby="button-addon2"
@@ -149,7 +149,7 @@ export default {
                             id="button-addon2"
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
+                                xmlns="https://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
                                 stroke-width="2.5"
