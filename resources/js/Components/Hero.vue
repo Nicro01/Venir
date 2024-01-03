3778286e10e579abf4661733725172ef9c9f1504
Nicro01 <nicro@edu.unifil.br>
2024-01-03 13:38:30 -0300

    Force HTTPS

--- resources/js/Components/Hero.vue
+++ resources/js/Components/Hero.vue
@@ -671,7 +671,7 @@ export default {
                     <div class="flex gap-5 items-center justify-center mt-10">
                         <a
                             type="button"
-                            class="inline-block px-10 rounded border-2 border-success pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-success transition duration-150 ease-in-out hover:border-success-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-success-600 focus:border-success-600 focus:text-success-600 focus:outline-none focus:ring-0 active:border-success-700 active:text-success-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                            class="cursor-pointer inline-block px-10 rounded border-2 border-success pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-success transition duration-150 ease-in-out hover:border-success-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-success-600 focus:border-success-600 focus:text-success-600 focus:outline-none focus:ring-0 active:border-success-700 active:text-success-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                             data-te-ripple-init
                             target="_blank"
                             @click="downloadVideo(this.info.data.hdplay)"
